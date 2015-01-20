11b88c074e9735e3f5b163ca48dee6c25bd4138c
xeronimus <xeronimus@gmail.com>
2015-01-20 17:19:59 +0100

    update docu for release 0.2.0

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -1,6 +1,6 @@
 NG_DOCS={
   "sections": {
-    "api": "API Reference"
+    "api": "Twigs Documentation"
   },
   "pages": [
     {
@@ -20,24 +20,6 @@ NG_DOCS={
       "moduleName": "twigs.activeRoute",
       "shortDescription": "In almost every webpage you&#39;d like to mark elements as active, if the current view matches their link. E.g., in a navigation Menu,",
       "keywords": "active active-route activeroute add api changes class complex css current currently directive element elements example flag highlighted href html item link listen mark match matches menu navigation ng-class regex route scope set true twg-active-route twgactive twigs url view webpage"
-    },
-    {
-      "section": "api",
-      "id": "twigs.choose.directive:twgChooseMultiple",
-      "shortName": "twgChooseMultiple",
-      "type": "directive",
-      "moduleName": "twigs.choose",
-      "shortDescription": "Creates a ui-select2 choose multi-select element.",
-      "keywords": "api array attribute attributes changed choice choices choose creates directive displayed dropdown element equal mapping model multi-select object properly selecting text twigs ui-select2 variable work"
-    },
-    {
-      "section": "api",
-      "id": "twigs.choose.directive:twgChooseSingle",
-      "shortName": "twgChooseSingle",
-      "type": "directive",
-      "moduleName": "twigs.choose",
-      "shortDescription": "Creates a ui-select2 choose dropdown element.",
-      "keywords": "api array attribute attributes changed choice choices choose creates directive displayed dropdown element equal mapping model object properly selecting text twigs ui-select2 variable work"
     },
     {
       "section": "api",
@@ -68,30 +50,12 @@ NG_DOCS={
     },
     {
       "section": "api",
-      "id": "twigs.dynamicSize.directive:twgDynamicHeight",
-      "shortName": "twgDynamicHeight",
-      "type": "directive",
-      "moduleName": "twigs.dynamicSize",
-      "shortDescription": "Allows to dynamically adjust the height of an element on every window resize event.",
-      "keywords": "adjust allows api directive div dynamically dynamicsize element event example height html resize twg-dynamic-height twigs window windowheight"
-    },
-    {
-      "section": "api",
-      "id": "twigs.dynamicSize.directive:twgDynamicWidth",
-      "shortName": "twgDynamicWidth",
-      "type": "directive",
-      "moduleName": "twigs.dynamicSize",
-      "shortDescription": "Allows to dynamically adjust the width of an element on every window resize event.",
-      "keywords": "adjust allows api directive div dynamically dynamicsize element event example html resize twg-dynamic-width twigs width window windowwidth"
-    },
-    {
-      "section": "api",
       "id": "twigs.flow.provider:FlowProvider",
       "shortName": "FlowProvider",
       "type": "object",
       "moduleName": "twigs.flow",
       "shortDescription": "In more complex applications we&#39;d like to chain different views into business processes, i.e. flows.",
-      "keywords": "$flowprovider $routeprovider access adds allowed allows angular api app application applications business chain completes complex config configuration controller controllers createflow creation current define definition definitions directly easy example flow flowprovider flows flwoid function guide html invoke javascript jump main match matches mechanism model module mywizard object path previous proceed process processes properties provider route routes second separate service share shared simple skip starting starts step stepconfig steps string switch templateurl third transitions twigs unique user usual valid var views wizard-like wizardctrl"
+      "keywords": "$routeprovider access adds allowed allows angular api app application applications business chain completes complex config configuration controller controllers createflow creation current define definition definitions directly easy example flow flowid flowprovider flows function guide html invoke javascript jump main match matches mechanism model module mywizard object path previous proceed process processes properties provider route routes second separate service share shared simple skip starting starts step stepconfig steps string switch templateurl third transitions twigs unique user usual valid var views wizard-like wizardctrl"
     },
     {
       "section": "api",
@@ -154,7 +118,15 @@ NG_DOCS={
       "type": "object",
       "moduleName": "twigs.menu",
       "shortDescription": "MenuProvider can be used to define menus globally which can later be used in multiple views.",
-      "keywords": "access accessible active activeroute additem adds angular api application attribute block child children class clicked complex config configuration configured content context corresponding create createmenu creates createsubmenu current currently default define defined defines depend described details determined direct directive directly display displayed enforced event example exist exists fa fa-desktop fa-lg fa-lock filtered find function functionality getmenuiteminmenu globally href html iconclass included instance item itemname itemoptions items iterate javascript key length link list loaded loading location login main_menu main_menu_home main_menu_orders main_menu_orders_new_order main_menu_orders_overview main_menu_settings main_menu_settings_roles main_menu_settings_users mainmenu mapped mark marks matches meet menu menu-hover menu-icon menu-name menuitem menuitems menuname menuoptions menuprovider menus methods module multiple myapp navigating needed nested ng-class object openable opened options order orders ordersmenu overview parent path pre predefined property protected protectedrouteprovider provider re-filtering referenced regex removed removemenu removes render restriction result returns role roles root route search searches security settings-menu settingsmenu setuserloadedeventname siblings signals strictly structure submenu submenu-label submenuitem submenus successful successfull template templateurl text translation triggers true twgmenu twigs undefined unique usage user userinitialized userloadedeventname users var views window x-ng-class x-ng-href x-ng-if x-ng-repeat"
+      "keywords": "access accessible active activeroute additem adds angular api application attribute block child children class clicked complex config configuration configured content context corresponding create createmenu creates createsubmenu current currently define defined defines depend described determined direct directive directly display displayed enforced example exist exists fa fa-desktop fa-lg fa-lock filtered find function functionality getmenuiteminmenu globally href html iconclass included instance item itemname itemoptions items iterate javascript key length link list location main_menu main_menu_home main_menu_orders main_menu_orders_new_order main_menu_orders_overview main_menu_settings main_menu_settings_roles main_menu_settings_users mainmenu mapped mark marks matches meet menu menu-hover menu-icon menu-name menuitem menuitems menuname menuoptions menuprovider menus methods module multiple myapp navigating needed nested ng-class object openable opened options order orders ordersmenu overview parent path predefined property protected protectedrouteprovider provider referenced regex removed removemenu removes render restriction result returns role roles root route search searches settings-menu settingsmenu siblings strictly structure submenu submenu-label submenuitem submenus template templateurl text translation true twgmenu twigs undefined unique usage users var views window x-ng-class x-ng-href x-ng-if x-ng-repeat"
+    },
+    {
+      "section": "api",
+      "id": "twigs.menu.service:MenuPermissionService",
+      "shortName": "MenuPermissionService",
+      "type": "object",
+      "moduleName": "twigs.menu",
+      "keywords": "api filter filtered filtermenuforrouterestrictions filters function items menu object protected resolves routes security service twigs"
     },
     {
       "section": "api",
@@ -163,51 +135,33 @@ NG_DOCS={
       "type": "object",
       "moduleName": "twigs.protectedRoutes",
       "shortDescription": "In an application that uses a permission model, we&#39;d like to protect some views from",
-      "keywords": "$routeprovider _authenticated_ _neededroles_ access additionally admin allows angular api app application appropriate authenticated changes check config configure controller define depends display don evaluate event forward function handled homectrl html javascript loader location logged main message model module needed neededroles note object permission prevented property protect protected protectedroute protectedrouteprovider protectedroutes provider registered required role roles route route-change routechangestart routes security set settingsctrl specific standard templateurl thrown true twigs unauthorized user user-roles var view views"
+      "keywords": "$routeprovider _authenticated_ _protection_ access admin allows angular api app application appropriate changes check config configure controller depends display don evaluate event forward function handled homectrl html javascript loader location logged main message model module note object permission permissions prevented property protect protected protectedroute protectedrouteprovider protectedroutes protection provider registered required route route-change routechangestart routes security set settingsctrl specific standard templateurl thrown true twigs unauthorized user var view views"
     },
     {
       "section": "api",
-      "id": "twigs.security.directive:twgSecureEnabled",
-      "shortName": "twgSecureEnabled",
+      "id": "twigs.security.directive:twgProtected",
+      "shortName": "twgProtected",
       "type": "directive",
       "moduleName": "twigs.security",
-      "shortDescription": "Enables a input field only if the given expression evaluates to true.",
-      "keywords": "allowed api arguments delete directive enables entity evaluates expression expressions field haspermission hasrole html input isauthenticated security text true twg-secure-enabled twigs type"
+      "shortDescription": "Protects a html element: either hides it or disables it if currently logged in user does not have",
+      "keywords": "api attributes currently directive disable disables element equivalent evaluated evaluation evaluator hidden hide hides html logged missing permission permissions protects registered security set true twg-protected twg-protected-disable twg-protected-hide twigs user"
     },
     {
       "section": "api",
-      "id": "twigs.security.directive:twgSecureShow",
-      "shortName": "twgSecureShow",
-      "type": "directive",
-      "moduleName": "twigs.security",
-      "shortDescription": "Shows an element only if given expression evaluates to true.",
-      "keywords": "allowed api arguments create directive element entity evaluates expression expressions haspermission hasrole html isauthenticated security true twg-secure-show twigs"
-    },
-    {
-      "section": "api",
-      "id": "twigs.security.provider:PermissionsProvider",
-      "shortName": "PermissionsProvider",
+      "id": "twigs.security.provider:AuthorizerProvider",
+      "shortName": "AuthorizerProvider",
       "type": "object",
       "moduleName": "twigs.security",
       "shortDescription": "",
-      "keywords": "$q $resource additional api args call decide defer deferred evaluating evaluation evaluator expected false fn form function ist javascript john load loader loading needed object pass permission permissionevaluatorfunction permissions permissionsprovider promise properties property provider receive registerpermissionevaluationfunction registers registeruserloader reject remember resolve resolves return role_1 role_2 roles second security server-response service true twigs user userloader userloadingpromise username users valid var waiting"
+      "keywords": "$q $resource additional api args authorizer authorizerprovider call decide defer deferred evaluating evaluation evaluator expected false fn foo form function getuser ist javascript john load loader loading needed object pass permission permissionevaluator permissions promise properties property provider receive registerpermissionevaluationfunction registerpermissionevaluator registers registeruserloader registeruserloaderfunction reject remember resolve resolves return second security server-response somecollaborator true twigs user userloader userloaderfunction userloadingpromise username users valid var waiting"
     },
     {
       "section": "api",
-      "id": "twigs.security.service:ExpressionEvaluator",
-      "shortName": "ExpressionEvaluator",
+      "id": "twigs.security.service:Authorizer",
+      "shortName": "Authorizer",
       "type": "object",
       "moduleName": "twigs.security",
-      "shortDescription": "ExpressionEvaluator is used by twigs.security directives.",
-      "keywords": "api currently directives evaluate evaluated evaluates expression expressionevaluator expressions function loaded match object permissions regular return security service twigs user validates"
-    },
-    {
-      "section": "api",
-      "id": "twigs.security.service:Permissions",
-      "shortName": "Permissions",
-      "type": "object",
-      "moduleName": "twigs.security",
-      "keywords": "api arguments authenticated call clears clearsecuritycontext current currently directives evaluator false function getuser haspermission hasrole holding invocation isauthenticated load loaded logged-in needed number object parameters passed permission promise registered return returns role rolename security securitycontext service true twigs user"
+      "keywords": "$rootscope api backend broadcast call check checks clears clearsecuritycontext current currently directives evaluator event false function getuser haspermission holding invocation isloggedin load loaded logged-in needed object ont passed pending permission permissions promise registered rejected resolve resolved resolves returns security securitycontext service specifies trigger true twigs user usercleared userload userloaded wait"
     },
     {
       "section": "api",
@@ -225,15 +179,37 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "twigs.tableRowClick",
       "shortDescription": "For a better user experience, we want to be able to react to mouseclicks anywhere on a table row, not just one link in a cell.",
-      "keywords": "additionally adds admin angular api app better bubble button cell change click clicks correctly directive dosomething elements events example experience expressions guard handle handlers hasrole href html ignoring invoke javascript link links listener location main method module mouse mouseclicks ng-click react routing row rows second secure security switches table table-row tablerowclick text third tr trigger twg-table-row-click twg-table-row-click-secure twigs url user users var x-ng-repeat"
+      "keywords": "additionally adds angular api app better bubble button cell change click clicks correctly directive dosomething elements events example experience handle handlers href html ignoring invoke javascript link links listener location main method module mouse mouseclicks ng-click react routing row rows second switches table tablerowclick text third tr trigger twg-table-row-click twigs url user users var x-ng-repeat"
     }
   ],
   "apis": {
     "api": true
   },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js"
+    ],
+    "styles": [],
+    "title": "Twigs Documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "animation": true
+  },
   "html5Mode": false,
+  "editExample": true,
   "startPage": "/api",
   "scripts": [
-    "angular.min.js"
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js"
   ]
 };
